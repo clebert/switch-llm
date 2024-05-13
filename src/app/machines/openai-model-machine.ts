7e33255429d3d586dfb228206bb0ade6897e77cb
Clemens Akens <clebert@me.com>
2024-05-13 21:01:52 +0200

    Add support for gpt-4o

--- src/app/machines/openai-model-machine.ts
+++ src/app/machines/openai-model-machine.ts
@@ -7,23 +7,29 @@ export type OpenaiModel = InferStateUnion<typeof openaiModelMachine>;
 
 const storageItem = createJsonStorageItem(
   `openaiModel`,
-  z.literal(`gpt-4`).or(z.literal(`gpt-4-turbo-preview`)).or(z.literal(`gpt-3.5-turbo`)),
+  z
+    .literal(`gpt-4o`)
+    .or(z.literal(`gpt-4-turbo`))
+    .or(z.literal(`gpt-4`))
+    .or(z.literal(`gpt-3.5-turbo`)),
 );
 
 export const openaiModelMachine = createMachine({
-  initialState: storageItem.value ?? `gpt-4-turbo-preview`,
+  initialState: storageItem.value ?? `gpt-4o`,
   initialValue: undefined,
 
   transformerMap: {
+    'gpt-4o': () => undefined,
+    'gpt-4-turbo': () => undefined,
     'gpt-4': () => undefined,
-    'gpt-4-turbo-preview': () => undefined,
     'gpt-3.5-turbo': () => undefined,
   },
 
   transitionsMap: {
-    'gpt-4': { toggle: `gpt-4-turbo-preview` },
-    'gpt-4-turbo-preview': { toggle: `gpt-3.5-turbo` },
-    'gpt-3.5-turbo': { toggle: `gpt-4` },
+    'gpt-4o': { toggle: `gpt-4-turbo` },
+    'gpt-4-turbo': { toggle: `gpt-4` },
+    'gpt-4': { toggle: `gpt-3.5-turbo` },
+    'gpt-3.5-turbo': { toggle: `gpt-4o` },
   },
 });
 
